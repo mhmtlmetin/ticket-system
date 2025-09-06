b6800e3244b1314acfe707966f0b3c0d5b12675d
mhmtlmetin <35113265+mhmtlmetin@users.noreply.github.com>
2025-09-06 19:10:52 +0300

    ticketcard düzeltildi

--- src/pages/userDashboard.jsx
+++ src/pages/userDashboard.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import { useGetAllTicketsQuery } from "../services/tickets.service";
 import TicketCard from "../components/ticketCard";
+import '../styles/UserDashboard.scss'
 
 
 export default function UserDashboard() {
   const { data, error, isLoading } = useGetAllTicketsQuery();
   console.log(data,"datas")
   return (
-    <div>
-      {data?.map((item) => (
+    <div className="user-container">
+        <div className="profile">
+
+        </div>
+    <div className="content">
+          {data?.map((item) => (
         <TicketCard key={item.id} request={item} />
       ))}
+    </div>
     </div>
   );
 }
